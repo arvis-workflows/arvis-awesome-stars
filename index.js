b183e94cae0c1dc2746b03d7bc3d1f7eea806df0
jopemachine <jopemachine@naver.com>
2021-10-23 13:59:34 +0900

    init

---
+++ index.js
@@ -0,0 +1,95 @@
+﻿const arvish = require('arvish');
+
+const id = process.env['githubUserId'];
+
+if (!id) {
+  arvish.error('githubUserId is not set!. please read README.md first.');
+  process.exit(1);
+}
+
+const url = `https://api.github.com/repos/${id}/awesome-stars/readme`;
+
+const repoInfoRegexp = /\- \[(?<maintainer>.*)\/(?<repoName>[A-Za-z0-9_.-]*)\](?<urlWithBracket>.*) \- (?<description>.*)/;
+
+const parseRepositoryInfo = (str) => {
+  let matched = str.match(repoInfoRegexp);
+
+  if (matched) {
+    return matched.groups;
+  }
+
+  return undefined;
+};
+
+const ban = [
+  'Contents',
+  'License'
+];
+
+const removeBraket = (str) => {
+  return str.substr(1, str.length - 2);
+}
+
+const transformData = (lang, repoInfo) => {
+  const title = `[${lang}] ${repoInfo.maintainer}/${repoInfo.repoName}`;
+  return {
+    title,
+    subtitle: repoInfo.description,
+    arg: removeBraket(repoInfo.urlWithBracket),
+    text: {
+      copy: title,
+      largetype: title,
+    },
+  };
+};
+
+(async () => {
+  const starredListRawString =
+    await arvish.fetch(url, { maxAge: 60 * 60 * 24 })
+      .then((response) => response.content)
+      .then((content) => Buffer.from(content, 'base64').toString('utf-8'));
+
+  const splits = starredListRawString.split(/(\#\#.*)/);
+
+  let lang = '(undefined)';
+  const data = [];
+
+  splits.forEach((line) => {
+    if (line.startsWith('##') && !ban.includes(line.split('## ')[1])) {
+      lang = line.split('##')[1].trim();
+    } else {
+      line.split('\n').forEach((repoInfoLine) => {
+        if (repoInfoLine.trim() !== '') {
+          const repoInfo = parseRepositoryInfo(repoInfoLine);
+
+          if (repoInfo) {
+            data.push(transformData(lang, repoInfo));
+          }
+        }
+      });
+    }
+  });
+
+  const result = data.filter((item) => {
+    let ret = false;
+    arvish.input.normalize().split(' ').forEach((word) => {
+      if (!ret) {
+        ret = item.title.toLowerCase().includes(word.toLowerCase()) || item.subtitle.toLowerCase().includes(word.toLowerCase());
+      }
+    });
+
+    return ret;
+  });
+
+  const summary = {
+    title: `${result.length} repositories were found.`,
+    subtitle: 'Press enter to open starred repositories\'s url',
+    arg: `https://github.com/${id}?tab=stars`
+  };
+
+  arvish.output([
+    summary,
+    ...result
+  ]);
+})();
+
---
+++ readme.md
@@ -0,0 +1,47 @@
+﻿# arvis-awesome-stars
+
+[![NPM version](https://badge.fury.io/js/arvis-awesome-stars.svg)](http://badge.fury.io/js/arvis-awesome-stars)
+[![npm download total](https://img.shields.io/npm/dt/arvis-awesome-stars)](https://www.npmjs.com/package/arvis-awesome-stars)
+[![license](https://img.shields.io/github/license/jopemachine/arvis-awesome-stars.svg)](https://github.com/jopemachine/arvis-awesome-stars/blob/master/LICENSE)
+[![PR's Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat)](http://makeapullrequest.com)
+[![GitHub issues](https://img.shields.io/github/issues/jopemachine/arvis-awesome-stars.svg)](https://GitHub.com/jopemachine/arvis-awesome-stars/issues/)
+
+
+> Search starred repository through 'awesome-stars'
+
+## Install
+
+```
+$ npm install --global arvis-awesome-stars
+```
+
+*Requires [Node.js](https://nodejs.org) 12+ and the Arvis [Powerpack](https://www.arvisapp.com/powerpack/).*
+
+## Setup
+
+### githubUserId
+
+Enter github user id.
+
+Make sure the user has [awesome-stars](https://github.com/maguowei/awesome-stars) repository containing readme.md
+
+## Usage
+
+In Arvis, type `star`, <kbd>Enter</kbd>, and your query.
+
+`star` searchs starred repositories's `maintainer name`, `repository name`, `repository description`, `programming language`.
+
+## Related
+
+- [starred](https://github.com/maguowei/starred)
+- [awesome-stars](https://github.com/maguowei/awesome-stars)
+
+## Icon sources
+
+This lib uses below icon sources
+
+<a target="_blank" href="https://icons8.com">Image</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+## License
+
+MIT © [jopemachine](https://github.com/jopemachine/arvis-awesome-stars)
